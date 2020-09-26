1f3a571355f4727b1c3a5671c0a1cabc41c4d870
Franco Aparicio <franco.aparicio@sgsdd-students.com>
2020-09-26 19:33:01 +0000

    Fixed Stuff

--- main.cs
+++ main.cs
@@ -5,36 +5,68 @@ using System.Linq;
 
 class MainClass {
   /*
-  Task - Nugget Sums
-  Chicken nuggets come in boxes of 4 , 6 , 9 and 20. Write a  program that takes in a number and returns if that exact   number of chicken nuggets can be reached using the box sizes given. So 2 would be no but 10 would be yes (6 and 4). ext   return all possible box size combinations. 
+  Summary: 
+  This program (I know can be written more efficiently)
+  finds all possible nugget packaging combinations by first
+  trying to use as many of the largest boxes as possible, 
+  then filling it in with as many as the smaller box size 
+  and so on. Then doing one less than the maximumm of the
+  largest box and so on.
+
+  Ex.
+    Amount: 71
+    Program:
+      71/20 = 3
+      Therefore:
+      3 boxes of 20 = 60 packed nuggets
+      71 - 60 = 11
+      Next:
+        11/9 = 1
+        Therefore:
+        1 box of 9 = 9 more packed nuggets
+        11 - 9 = 2
+        2 < 4, Therefore program moves back to the next highest
+        box size (9). However it used the lowest possible amount
+        of this size (1) already. Therefore it moves back to the
+        next highest box size (20)
+        Since it last used 3 boxes of 20, it will now try 2 boxes
+        2 boxes of 20 = 40 packed nuggets
+        71 - 40 = 31
+        Next:
+          31/9 = 3
+          Therefore:
+          3 boxes of 9 = 27 more packed nuggets
+          31 - 27 = 4
+          Next:
+            4/6 = 0
+            Therefore:
+            0 boxes of 6 = 0 more packed nuggets
+            4 - 0 = 4
+            Next:
+              4/4 = 1
+              Therefore:
+              1 box of 4 = 4 more packed nuggets
+              4 - 4 = 0
+              Therefore:
+              The combination of 2 boxes of 20, 3 boxes of 9, and
+              1 box of 4 (20, 20, 9, 9, 9, 4) is added to the list of possible combinations.
+              Next:
+                It moves back to the next highest box size possible
+                (9).
+                2 boxes of 9 = 18 more packed nuggets
+                31 - 18 = 13
+                (And this continues for every box size)
   */
   public static void Main (string[] args) {
     Write ("\nEnter the desired number of nuggets: ");
     int amount = Convert.ToInt32(ReadLine());
     // Store the list of box combinations returned by the method
-    // and initialize another list to record which combinations
-    // have already been outputed (to avoid duplicates).
     List<List<int>> combos = FindCombos(amount);
-    List<List<int>> printed = new List<List<int>>();
     // bool stays false if there are no possible box combinations
     bool possible = false;
-    // Print all possible combinations avoiding duplicates
+    // Print all possible combinations
     WriteLine ("\n\nCombinations:");
     foreach (List<int> combo in combos) {
-      if (combo.Count == 0) {
-        continue;
-      }
-      bool skip = false;
-      foreach (List<int> list in printed) {
-        if (list.SequenceEqual(combo)) {
-          skip = true;
-          break;
-        }
-      }
-      if (skip) {
-        continue;
-      }
-      printed.Add(combo);
       Write ("(");
       foreach (int box in combo) {
         Write(box + ", ");
@@ -42,85 +74,111 @@ class MainClass {
       }
       Write("\b\b)\n");
     }
+    // If the boolean was never changed to true 
+    // (if no combination was found), prints out "None"
     if (!possible) {
       WriteLine("None");
     }
   }
   public static List<List<int>> FindCombos(int amount) {
-    // Initialize the list of possible combinations
+    // Initializes list of possible combinations
+    // that the method will return
     List<List<int>> combos = new List<List<int>>();
-    // sets up a copy of the amount of nuggets to package
-    // and keeps track of the list indecies to avoid overwriting
-    int amountCopy;
-    int possibility = 0;
-    // this list defines at which box size it will start
-    // packaging nuggets in
-    int[] starts = new int[] {20, 9, 6, 4};
-    foreach (int start in starts) {
-      // resets the amount copy for next combination
-      amountCopy = amount;
-      // adds new list for this combination in the 2D list
-      combos.Add (new List<int>());
-      // This will keep allocating the largest boxes of nuggets
-      // possible until there are no nuggets left or
-      // they cannot be packaged properly
-      while (amountCopy > 0) {
-        if (amountCopy >= 20 && start == 20) {
-          combos[possibility].Add (20);
-          amountCopy -= 20;
+    // Array has all the box size possibilities
+    int[] sizes = new int[]{20, 9, 6, 4};
+    // For every size in the list of box sizes...
+    foreach (int size1 in sizes) {
+      // For as many times as it is possible to use this box size
+      // for this amount of nuggests...
+      for (int times1 = amount/size1; times1 > 0; times1--) {
+        // Copies the amount in order to perform operations without
+        // losing the original amount
+        int amount1 = amount;
+        // Initialises list to store the box sizes being used for this combination
+        List<int> combination1 = new List<int>();
+        // Adds as many boxes of the current size as defined by the loop above into the combination list and subtracts the amount of nuggets being stored in those boxes from the copied amount
+        for (int t = times1; t > 0; t--) {
+          combination1.Add (size1);
+          amount1 -= size1;
         }
-        else if (amountCopy >= 9 && start >= 9) {
-          combos[possibility].Add (9);
-          amountCopy -= 9;
+        // If the boxes can fit the exact number of nuggets, add this combination to the list of of possible combinations and continue to the next iteration of the loop (next amount of boxes)
+        if (amount1 == 0) {
+          combos.Add(combination1);
+          continue;
         }
-        else if (amountCopy >= 6 && start >= 6) {
-          combos[possibility].Add (6);
-          amountCopy -= 6;
+        // If the remaining nuggets can't fill another box, skip to the next iteration of the loop
+        else if (amount1 < 4) {
+          continue;
         }
-        else if (amountCopy >= 4 && start >= 4) {
-          combos[possibility].Add (4);
-          amountCopy -= 4;
-        }
+        // If there are still nuggets that can fit in smaller boxes...
         else {
-          if (amountCopy != 0) {
-            combos[possibility].Clear();
+          foreach (int size2 in sizes) {
+            if (size2 >= size1) {
+              continue;
+            }
+            for (int times2 = amount1/size2; times2 > 0; times2--) {
+              int amount2 = amount1;
+              List<int> combination2 = new List<int>(combination1);
+              for (int t = times2; t > 0; t--) {
+                combination2.Add (size2);
+                amount2 -= size2;
+              }
+              if (amount2 == 0) {
+                combos.Add(combination2);
+                continue;
+              }
+              else if (amount2 < 4) {
+                continue;
+              }
+              else {
+                foreach (int size3 in sizes) {
+                  if (size3 >= size2) {
+                    continue;
+                  }
+                  for (int times3 = amount2/size3; times3 > 0; times3--) {
+                    int amount3 = amount2;
+                    List<int> combination3 = new List<int>(combination2);
+                    for (int t = times3; t > 0; t--) {
+                      combination3.Add (size3);
+                      amount3 -= size3;
+                    }
+                    if (amount3 == 0) {
+                      combos.Add(combination3);
+                      continue;
+                    }
+                    else if (amount3 < 4) {
+                      continue;
+                    }
+                    else {
+                      foreach (int size4 in sizes) {
+                        if (size4 >= size3) {
+                          continue;
+                        }
+                        for (int times4 = amount3/size4; times4 > 0; times4--) {
+                          int amount4 = amount3;
+                          List<int> combination4 = new List<int>(combination3);
+                          for (int t = times4; t > 0; t--) {
+                            combination4.Add (size4);
+                            amount4 -= size4;
+                          }
+                          if (amount4 == 0) {
+                            combos.Add(combination4);
+                            continue;
+                          }
+                          else if (amount3 < 4) {
+                            continue;
+                          }
+                          else {}
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
           }
-          break;
         }
       }
-      possibility++;
-    }
-    // This will keep allocating boxes of nuggets from 
-    // smallest to largest until there are no nuggets left
-    // or they cannot be packaged properly
-    foreach (int start in starts) {
-      amountCopy = amount;
-      combos.Add (new List<int>());
-      while (amountCopy > 0) {
-        if (amountCopy >= 20 && start == 20) {
-          combos[possibility].Add (20);
-          amountCopy -= 20;
-        }
-        if (amountCopy >= 9 && start >= 9) {
-          combos[possibility].Add (9);
-          amountCopy -= 9;
-        }
-        if (amountCopy >= 6 && start >= 6) {
-          combos[possibility].Add (6);
-          amountCopy -= 6;
-        }
-        if (amountCopy >= 4 && start >= 4) {
-          combos[possibility].Add (4);
-          amountCopy -= 4;
-        }
-        else {
-          if (amountCopy != 0) {
-            combos[possibility].Clear();
-          }
-          break;
-        }
-      }
-      possibility++;
     }
     return combos;
   }
